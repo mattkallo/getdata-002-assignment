00ebfdbdcfc985da9977a4c239a02b704b8ea95c
mattshub <mattshub@users.noreply.github.com>
2014-04-27 09:11:21 -0400

    Cleaned-up

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,14 @@
 
-## Functions
+## R Program : Utility to read HAR Dataset and perform clean-up operations to create the Tidy 
+## Dataset. This program reads the provided test and train set data; merge data points for 
+## each set and merge both sets together as a single dataset. A summarized dataset is created 
+## with average of each variable
+
 ## Function to read and filter Test Data
+## - Reads Subjects, X and Y Information
+## - Merges observations with descriptive activity names
+## - Merges with subjects data for each observation
+## - Returns merged Test Data
 getTestData <- function(activityLabels){        
         ## Read test data
         testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
@@ -15,6 +23,10 @@ getTestData <- function(activityLabels){
 }
 
 ## Function to read and filter Train Data
+## - Reads Subjects, X and Y Information
+## - Merges observations with descriptive activity names
+## - Merges with subjects data for each observation
+## - Returns merged Train Data
 getTrainData <- function(activityLabels){
         ## Read training data
         trainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
@@ -29,6 +41,8 @@ getTrainData <- function(activityLabels){
 }
 
 ## Function to read and add descriptive labels
+## - Reads the labels data provided with the dataset
+## - Modifies abreviations in labels to create more meaningful variable descriptions
 getFeatureLabels <- function(){
         labels <- read.table("UCI HAR Dataset/features.txt")
         labels$V3 <- labels$V2
@@ -60,28 +74,35 @@ getFeatureLabels <- function(){
         labels
 }
 
-## Read meta data
+## Read Activity Labels
 activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
 names(activityLabels) <- c("ActivityID","ActivityDescription")
+
+## Get feature labels with descriptive names
 featureLabels <- getFeatureLabels()
+
+## Get Merged Test Data
 testData <- getTestData(activityLabels)
+
+## Get merged Train Data
 trainData <- getTrainData(activityLabels)
+
+## Combine both Test and Train Datasets
 combinedData <- rbind(trainData, testData)
-print(ncol(combinedData))
 names(combinedData) <- c("Subject","ActivityID","ActivityDescription",as.character(featureLabels$V2))
+
+## Subset Mean and Standard Deviation Columns
 stdMeanColumns <- featureLabels[grep("mean\\(\\)|std\\(\\)", featureLabels$V2),]
-ass <- c(as.character(stdMeanColumns[,2]))
-print(ass)
 filteredData <- combinedData[,c("Subject","ActivityID","ActivityDescription",c(as.character(stdMeanColumns[,2])))]
 names(filteredData) <- c("Subject","ActivityID","ActivityDescription",as.character(stdMeanColumns$V3))
-write.table(filteredData, file ="HARTidyDataset1.txt",row.names=FALSE,sep=" ", append=FALSE)
+##write.table(filteredData, file ="HARTidyDataset1.csv",row.names=FALSE,sep=",", append=FALSE)
 
 #Second Data Set
-print(ncol(combinedData))
 names(combinedData) <- c("Subject","ActivityID","ActivityDescription",as.character(featureLabels$V3))
 combinedData[, c(4:564)] <- sapply(combinedData[, c(4:564)], as.character)
 combinedData[, c(4:564)] <- sapply(combinedData[, c(4:564)], as.numeric)
 summarizedData <- aggregate(combinedData[, c(4:564)], by = list(combinedData$Subject, combinedData$ActivityID,combinedData$ActivityDescription), FUN = "mean", rm.na=TRUE)
 colnames(summarizedData)[c(1,2,3)] <- c("Subject","ActivityID","ActivityDescription")
-write.table(summarizedData, file ="summarizedData.csv",row.names=FALSE,sep=",", append=FALSE)
+write.table(summarizedData, file ="HARTidyDataset2.csv",row.names=FALSE,sep=",", append=FALSE)
 
+## End of Program
